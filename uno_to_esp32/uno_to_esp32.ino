52c94efb19cb77de1dbfb9a8334c8003162447f5
zxckoii <145819507+prdhk2@users.noreply.github.com>
2023-09-25 15:06:47 +0700

    Delete uno_to_esp32/uno_to_esp32.ino

--- uno_to_esp32/uno_to_esp32.ino
+++
@@ -1,8 +0,0 @@
-void setup() {
-  Serial.begin(9600);  // Inisialisasi komunikasi serial dengan kecepatan 9600 bps
-}
-
-void loop() {
-  Serial.println("Hello from Arduino Uno!");  // Mengirim pesan melalui komunikasi serial
-  delay(1000);  // Delay 1 detik
-}
